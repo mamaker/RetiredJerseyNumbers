16753be26ea5d9248b3ad3b5890254a58c129a1d
mamaker <madhuvasudevan@hotmail.com>
2015-05-22 01:29:27 -0500

    Done!

--- RetiredJerseyNumbers/Form1.Designer.cs
+++ RetiredJerseyNumbers/Form1.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.yearLabel = new System.Windows.Forms.Label();
+            this.number = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "was worn by";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.nameLabel.Location = new System.Drawing.Point(302, 42);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(199, 23);
+            this.nameLabel.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(507, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "and retired in";
+            // 
+            // yearLabel
+            // 
+            this.yearLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.yearLabel.Location = new System.Drawing.Point(624, 42);
+            this.yearLabel.Name = "yearLabel";
+            this.yearLabel.Size = new System.Drawing.Size(100, 23);
+            this.yearLabel.TabIndex = 5;
+            // 
+            // number
+            // 
+            this.number.FormattingEnabled = true;
+            this.number.Location = new System.Drawing.Point(78, 38);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(108, 24);
+            this.number.TabIndex = 6;
+            this.number.SelectedIndexChanged += new System.EventHandler(this.number_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(861, 101);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.yearLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Retired Jersey Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label yearLabel;
+        private System.Windows.Forms.ComboBox number;
     }
 }
 
